d753f859dc1687257b708a9fc8feb1a6ce7f6f8b
Joshua Auriemma <allocate@gmail.com>
2014-07-21 13:38:40 -0400

    Re-write code to simply compare statute titles

--- data_fun.py
+++ data_fun.py
@@ -1,10 +1,11 @@
 # -- coding: utf-8 --
 import re, pprint, pickle, HTMLParser
-
+from pprint import pprint
 fun = pickle.load(open("similar_dict.p"))
-text_file = open('Output.txt', 'w')
+#text_file = open('Output.txt', 'w')
 
-
+# for i in fun:
+# 	print str(i) + '\t' + str(fun[i]['title_similar']) + '\t' + str(fun[i]['text_similar'])
 
 # for i in fun:
 # 	if fun[i]['title_similar'] < 90:
@@ -16,18 +17,18 @@ text_file = open('Output.txt', 'w')
 # 		except Exception:
 # 			print 'Problem with ' + i
 
-statuteNum = '43-39-1'
+statuteNum = '41-6-10'
 #print "Inapplicability of \xa7\xa7\xa037-5-16 and 37-5-17 where only method of sales is thr...".encode('string_escape')
 #print "Inapplicability of \xa7\xa7\xa037-5-16 and 37-5-17 where only method of sales is thr...".decode('utf-8', 'ignore')
 
-print fun[statuteNum]
+# print fun[statuteNum]
 
-# print fun[statuteNum]['ddisk_title']
-# print fun[statuteNum]['web_title']
+#print fun[statuteNum]['ddisk_title']
+#print fun[statuteNum]['web_title']
 
-# print fun[statuteNum]['ddisk_text'].decode('utf-8', 'ignore')
-# print '\n'
-# print fun[statuteNum]['web_text'].encode('string_escape')
+pprint (fun[statuteNum]['ddisk_text'])
+print '\n'
+pprint (fun[statuteNum]['web_text'])
 
 
-text_file.close
+#text_file.close
